728b947be8b19a92be2404af55bf3b0c87f39d19
Kavin Gnanapandithan <kavin.balag@gmail.com>
2024-02-18 11:45:25 -0500

    Decrease time delay

--- src/main.rs
+++ src/main.rs
@@ -107,7 +107,7 @@ mod app {
     #[task(shared = [x_kalman, y_kalman, timer] ,local = [imu, i2c], priority = 1)]
     async fn filter_imu_data(mut ctx: filter_imu_data::Context) {
         //rprintln!("filter");
-        let delta_sec = 0.2;
+        let delta_sec = 0.012;
 
         let mut accel_data:[f32; 3] = [0.0, 0.0, 0.0];
         let mut gyro_data:[f32; 3] = [0.0, 0.0, 0.0];
@@ -132,7 +132,7 @@ mod app {
         });
 
         ctx.shared.timer.lock(|f| {
-            f.start(15.millis()).unwrap();
+            f.start(12.millis()).unwrap();
         });
     }
 
